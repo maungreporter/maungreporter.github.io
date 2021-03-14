54deaf0dd6e45a539b56e45d61226a80b04bdd45
maungrepo <maungrepoter@gmail.com>
2021-03-14 22:05:26 +0900

    gsp api added

--- assets/js/main-vue.js
+++ assets/js/main-vue.js
@@ -1,8 +1,27 @@
-console.log("martyrList" + martyrList);
+var martyrList ;
+
+(async function(){
+
+    await fetch('https://sheets.googleapis.com/v4/spreadsheets/1PYlfnHxUJFc_GYtFCpcvAIb3QbxYtBGQq9Ra2eltT3g/values/[MM]Detail Cases?key=AIzaSyBuoa3iAy6JtfpBUpcqL4k1gsrMT631TPw')
+    .then(res=>res.json())
+    .then(response =>{
+        martyrList = response.values;
+        
+        console.log("martyrList" + martyrList);
+        martyrVM.martyrList = martyrList;
+    }).catch(err => {
+
+    });
+    
+    
+   
+
+})();
+
 Vue.component('martyr-list', {
     props: ['todo'],
     template: '<li>{{ todo[0] }}</li>'
-  })
+})
 
 var martyrVM = new Vue({
     el:'#martyr',
@@ -10,3 +29,4 @@ var martyrVM = new Vue({
         martyrList: martyrList,
     }
 })
+
