84c94ec010e8caf5711ee78a431f7f8c40a667df
maungrepo <maungrepoter@gmail.com>
2021-04-03 16:32:52 +0900

    adding map

---
+++ assets/js/mapdata.js
@@ -0,0 +1,138 @@
+var cc = martyrVM.underEighteen
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+    //General settings
+		width: "responsive", //or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    pop_ups: "detect",
+    
+		//State defaults
+		state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+		//Location defaults
+		location_description: "Location description",
+    location_url: "",
+    location_color: "#a11f21",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "circle",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+		//Label defaults
+		label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+		//Zoom settings
+		zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+		//Popup settings
+		popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+		//Advanced settings
+		div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website"
+  },
+  state_specific: {
+    MMR2473: {
+      name: "ကယား",
+      description: "ပြည်နယ်",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    MMR3266: {
+      name: "ကရင်",
+      description: "ပြည်နယ်",
+    },
+    MMR3267: {
+      name: "မန္တလေး",
+      description: "တိုင်း",
+    },
+    MMR3268: {
+      name: "ပဲခူး",
+      description: "တိုင်း",
+    },
+    MMR3269: {
+      name: "ရန်ကုန်",
+      description: "တိုင်း",
+    },
+    MMR3270: {
+      name: "မွန်",
+      description: "ပြည်နယ်",
+    },
+    MMR3273: {
+      name: "ရခိုင်",
+      description: "ပြည်နယ်",
+    },
+    MMR3274: {
+      name: "Chin",
+      description: "ပြည်နယ်",
+    },
+    MMR3275: {
+      name: "ဧရာဝတီ",
+      description: "တိုင်း",
+    },
+    MMR3276: {
+      name: "မကွေး",
+      description: "တိုင်း",
+    },
+    MMR3277: {
+      name: "ရှမ်း",
+      description: "ပြည်နယ်",
+    },
+    MMR3279: {
+      name: "တနင်္သာရီ",
+      description: "တိုင်း",
+    },
+    MMR3296: {
+      name: "ကချင်",
+      description: "ပြည်နယ်",
+    },
+    MMR3302: {
+      name: "စကိုင်း",
+      description: "တိုင်း",
+    }
+  },
+  locations: {
+  
+    
+  }
+};
+
