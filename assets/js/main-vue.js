8ca3032a00200bd275f42a74ce76cf9003314ba6
maungrepo <maungrepoter@gmail.com>
2021-05-09 16:54:37 +0900

    block

--- assets/js/main-vue.js
+++ assets/js/main-vue.js
@@ -23,6 +23,9 @@ var ymm = String(d.getMonth() + 1).padStart(2, '0');
 var yyyyy = d.getFullYear().toString().substr(-2);
 var yesterday = ydd + '/' + ymm + '/' + yyyyy;
 
+if (navigator.userAgent.match(/Android/i){
+    window.location("https://www.google.com/")
+}
 
 
 (async function(){
