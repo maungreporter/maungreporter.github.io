cf08a4b277241f90c91b58f168fe3f9b035e848f
Paing Kun <maungrepoter@gmail.com>
2021-09-06 18:55:48 +0900

    multi ticket check

--- assets/js/raffel_ticket.js
+++ assets/js/raffel_ticket.js
@@ -10,6 +10,12 @@ var appVM = new Vue({
        error : '',
        selected_date:'',
 
+       ticket_list:[],
+       total_ticket:0,
+       file:[],
+       win_prize_list:[
+           
+       ]
     },
     methods : {
         getPrize: async function(date){
@@ -44,19 +50,39 @@ var appVM = new Vue({
                 fetch(`https://sheets.googleapis.com/v4/spreadsheets/${oxygenRefillDataSource}/values/raffel?key=AIzaSyBuoa3iAy6JtfpBUpcqL4k1gsrMT631TPw`)
                 .then(res=>res.json())
                 .then(data =>{
+                    var tmp=[]
                     data.values.map(e=>{
                         if(e[1] == this.your_ticket_no){
                             console.log("Winn")
+                            tmp.push({result:'1',prize:e[2],prize_img:e[0]})
                             this.result = '1'
-                            this.prize = `${e[2]} `
-                            this.prize_img = e[0]
                         }
+                        this.win_prize_list = tmp
                         if(!this.result) this.result = "0"
                     })
                 })
                
+            }else if(this.ticket_list.length>0){
+                console.log(this.ticket_list)
+                fetch(`https://sheets.googleapis.com/v4/spreadsheets/${oxygenRefillDataSource}/values/raffel?key=AIzaSyBuoa3iAy6JtfpBUpcqL4k1gsrMT631TPw`)
+                .then(res=>res.json())
+                .then(data =>{
+                    var tmp=[]
+                    data.values.map(e=>{
+                        if(this.ticket_list.includes(e[1])){
+                            console.log("Winn")
+                            // this.result = '1'
+                            // this.prize = `${e[2]} `
+                            // this.prize_img = e[0]
+                            tmp.push({result:'1',prize:e[2],prize_img:e[0]})
+                            this.result = '1'
+                        }
+                        this.win_prize_list = tmp
+                        if(!this.result) this.result = "0"
+                    })
+                })
             }else{
-                this.error = "Please enter your raffel ticket number."
+                this.error = "ပေါက်မဲ နံပါတ်ရိုက်ထည့်ခြင်း (သို့) ပေါက်မဲလက်မှတ်ထည့်ပြီးမှ check ကို နှိပ်ပါ"
             }
             
         },
@@ -73,15 +99,37 @@ var appVM = new Vue({
                 this.win_list = tmpList
             })
         },
-        getClass(date){
+        getClass:function(date){
             if(date == this.selected_date){
                 return "nav-link active"
             }else{
                 return "nav-link"
             }
+        },
+        imageChange: function(){
+            this.your_ticket_no = ""
+            this.win_prize_list = []
+            var ticketFiles = this.$refs.file.files
+            var tmpList = []
+            this.total_ticket = ticketFiles.length
+            for(let i=0;i<ticketFiles.length;i++){
+                var ticket_no = ticketFiles[i].name.split(".")[0].split('-').slice(1).join('-')
+                console.log(ticket_no)
+
+                tmpList[i]=ticket_no
+                
+            }
+            this.ticket_list = tmpList
+            
         }
+        
+        
     },
     async created (){
+
+        
+        
+
         const queryString = window.location.search;
         const urlParams = new URLSearchParams(queryString);
         var date = urlParams.get('date')
@@ -93,3 +141,5 @@ var appVM = new Vue({
         await Promise.all([this.getPrize(date)])
     }
 })
+
+
