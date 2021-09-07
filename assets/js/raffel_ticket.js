9460980c1b968ca3d8694cf3b3bd7f7b24ae03f2
Paing Kun <maungrepoter@gmail.com>
2021-09-07 21:55:24 +0900

    Ticket Error

--- assets/js/raffel_ticket.js
+++ assets/js/raffel_ticket.js
@@ -44,45 +44,48 @@ var appVM = new Vue({
            
         },
         check: function(){
-            this.result = ""
-            this.error = ""
-            if(this.your_ticket_no){
-                fetch(`https://sheets.googleapis.com/v4/spreadsheets/${oxygenRefillDataSource}/values/raffel?key=AIzaSyBuoa3iAy6JtfpBUpcqL4k1gsrMT631TPw`)
-                .then(res=>res.json())
-                .then(data =>{
-                    var tmp=[]
-                    data.values.map(e=>{
-                        if(e[1] == this.your_ticket_no){
-                            console.log("Winn")
-                            tmp.push({result:'1',prize:e[2],prize_img:e[0]})
-                            this.result = '1'
-                        }
-                        this.win_prize_list = tmp
-                        if(!this.result) this.result = "0"
+            if(!this.error){
+                this.result = ""
+                this.error = ""
+                if(this.your_ticket_no){
+                    fetch(`https://sheets.googleapis.com/v4/spreadsheets/${oxygenRefillDataSource}/values/raffel?key=AIzaSyBuoa3iAy6JtfpBUpcqL4k1gsrMT631TPw`)
+                    .then(res=>res.json())
+                    .then(data =>{
+                        var tmp=[]
+                        data.values.map(e=>{
+                            if(e[1] == this.your_ticket_no){
+                                console.log("Winn")
+                                tmp.push({result:'1',prize:e[2],prize_img:e[0]})
+                                this.result = '1'
+                            }
+                            this.win_prize_list = tmp
+                            if(!this.result) this.result = "0"
+                        })
                     })
-                })
-               
-            }else if(this.ticket_list.length>0){
-                console.log(this.ticket_list)
-                fetch(`https://sheets.googleapis.com/v4/spreadsheets/${oxygenRefillDataSource}/values/raffel?key=AIzaSyBuoa3iAy6JtfpBUpcqL4k1gsrMT631TPw`)
-                .then(res=>res.json())
-                .then(data =>{
-                    var tmp=[]
-                    data.values.map(e=>{
-                        if(this.ticket_list.includes(e[1])){
-                            console.log("Winn")
-                            // this.result = '1'
-                            // this.prize = `${e[2]} `
-                            // this.prize_img = e[0]
-                            tmp.push({result:'1',prize:e[2],prize_img:e[0]})
-                            this.result = '1'
-                        }
-                        this.win_prize_list = tmp
-                        if(!this.result) this.result = "0"
+                   
+                }else if(this.ticket_list.length>0){
+                    console.log(this.ticket_list)
+                    fetch(`https://sheets.googleapis.com/v4/spreadsheets/${oxygenRefillDataSource}/values/raffel?key=AIzaSyBuoa3iAy6JtfpBUpcqL4k1gsrMT631TPw`)
+                    .then(res=>res.json())
+                    .then(data =>{
+                        var tmp=[]
+                        data.values.map(e=>{
+                            if(this.ticket_list.includes(e[1])){
+                                console.log("Winn")
+                                // this.result = '1'
+                                // this.prize = `${e[2]} `
+                                // this.prize_img = e[0]
+                                tmp.push({result:'1',prize:e[2],prize_img:e[0]})
+                                this.result = '1'
+                            }
+                            this.win_prize_list = tmp
+                            if(!this.result) this.result = "0"
+                        })
                     })
-                })
-            }else{
-                this.error = "ပေါက်မဲ နံပါတ်ရိုက်ထည့်ခြင်း (သို့) ပေါက်မဲလက်မှတ်ထည့်ပြီးမှ check ကို နှိပ်ပါ"
+                }else{
+                    this.error = "1"
+                }
+                
             }
             
         },
@@ -107,6 +110,7 @@ var appVM = new Vue({
             }
         },
         imageChange: function(){
+            this.error = ""
             this.your_ticket_no = ""
             this.win_prize_list = []
             var ticketFiles = this.$refs.file.files
@@ -115,12 +119,21 @@ var appVM = new Vue({
             for(let i=0;i<ticketFiles.length;i++){
                 var ticket_no = ticketFiles[i].name.split(".")[0].split('-').slice(1).join('-')
                 console.log(ticket_no)
+                if(!this.checkTicket(ticket_no)){
+                    this.error = "2"
+                    this.ticket_list = []
+                    break
+                }
 
                 tmpList[i]=ticket_no
                 
             }
             this.ticket_list = tmpList
+            // this.error = ""
             
+        },
+        checkTicket: function(no){
+            return (no.split('-').length!=4) ? 0 : 1;
         }
         
         
